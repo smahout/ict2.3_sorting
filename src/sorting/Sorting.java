399dc5ee2b31b97ad229da22e80c34e3ec79e938
Wouter <wouterhoutsma@gmail.com>
2017-03-23 22:19:52 +0100

    Changed the way the histogram looks and added buttons

--- src/sorting/Sorting.java
+++ src/sorting/Sorting.java
@@ -1,23 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sorting;
 
 import java.util.ArrayList;
-//import javafx.application.Application;
-//import javafx.event.ActionEvent;
-//import javafx.event.EventHandler;
-//import javafx.scene.Scene;
-//import javafx.scene.control.Button;
-//import javafx.scene.layout.StackPane;
-//import javafx.stage.Stage;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -27,10 +17,10 @@ import javafx.stage.Stage;
  * @author Wouter
  */
 public class Sorting extends Application {
-    private final int n = 50;
+    private final int n = 60;
     private final int min = 1;
     private final int max = 100;
-    private final int width = 600;
+    private final int width = 900;
     private final int height = 400;
    
     private ArrayList<Integer> data;
@@ -50,11 +40,19 @@ public class Sorting extends Application {
         Scene scene = new Scene(root, width, height);
         
         generateNewHistogram();
-        for(Rectangle r: histogram){
-            System.out.println(r);
-            root.getChildren().add(r);
-        }
-        
+        //drawHistogramOnPane(root);
+        Button step = new Button("Sort step");
+        step.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e){
+                generateNewHistogram();
+                drawHistogramOnPane(root);
+            }
+        });
+        Button play = new Button("Play");
+        step.setLayoutX(100);
+        Button stop = new Button("Stop");
+        root.getChildren().add(step);
+        root.getChildren().add(play);
         primaryStage.setTitle("Sorting algorithms");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -64,25 +62,32 @@ public class Sorting extends Application {
         for(Rectangle r : histogram){
             root.getChildren().remove(r);
         }
+        
+        double offset_height = height * 0.15; 
+        double offset_width = width * 0.05;      
+        double pixel_height = (height / 5 * 4) / (max - min);
+        double rectangle_width = ((width - offset_width * 2) - (n - 1) * 3) / n;
         histogram = new ArrayList();
+      
         for(Integer i : data){
-            // Max height = 80% of height
-            // Offset = 10% from bottom, 10% from top
-            int offset_height = height / 10;    // 10%
-            int offset_width = width / 20;      // 5%
-            double pixel_height = (height / 5 * 4) / (max - min);
-            double rectangle_width = (width - offset_width * 2) / n;
             double rectangle_height = pixel_height * i;
-            double x = offset_width + histogram.size() * rectangle_width;
-            double y = (height / 5 * 4) + offset_height - rectangle_height;
+            double x = offset_width + histogram.size() * (rectangle_width + 3);
+            double y = (height - offset_height * 2) + offset_height - rectangle_height;
             Rectangle r = new Rectangle(x,y,rectangle_width, rectangle_height);
-            r.setFill(Color.BLUE);
+            r.setFill(Color.WHITE);
+            r.setStroke(Color.RED);
             histogram.add(r);            
         }
     }
-    /**
-     * @param args the command line arguments
-     */
+    
+    private void drawHistogramOnPane(Pane p){
+        for(Rectangle r: histogram){
+            
+            p.getChildren().add(r);
+        }
+    }
+
+
     public static void main(String[] args) {        
         launch(args);
     }
